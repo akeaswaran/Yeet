61b007fa78f2b642c30962b4db6274fecafe9b7a
akeaswaran <akeaswaran@me.com>
2015-01-13 10:56:32 -0500

    Initial Commit

---
+++ yeet/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  yeet
+//
+//  Created by Akshay Easwaran on 1/13/15.
+//  Copyright (c) 2015 Akshay Easwaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ yeet/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  yeet
+//
+//  Created by Akshay Easwaran on 1/13/15.
+//  Copyright (c) 2015 Akshay Easwaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
